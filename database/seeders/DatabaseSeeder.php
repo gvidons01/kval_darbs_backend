e4b4a4b5aa66b2b856cd61d8a393747c6e8e41d5
Gvido <me@email.com>
2022-11-03 13:15:09 +0200

    database seeder for groups

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use app\Models\group;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +13,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      \DB::table('groups')->insert([
+        'ID' => 1,
+        'group_name' => 'Transports',
+        'image' => file_get_contents("images/groups/car.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 2,
+        'group_name' => 'Nekustamais īpašums',
+        'image' => file_get_contents("images/groups/home.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 3,
+        'group_name' => 'Elektrotehnika',
+        'image' => file_get_contents("images/groups/responsive.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 4,
+        'group_name' => 'Apģērbs',
+        'image' => file_get_contents("images/groups/tshirt.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 5,
+        'group_name' => 'Mājai',
+        'image' => file_get_contents("images/groups/shelf.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 6,
+        'group_name' => 'Bērniem',
+        'image' => file_get_contents("images/groups/baby-boy.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 7,
+        'group_name' => 'Atpūtai/hobijiem',
+        'image' => file_get_contents("images/groups/hiking.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 8,
+        'group_name' => 'Lauksaimniecībai',
+        'image' => file_get_contents("images/groups/tractor.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 9,
+        'group_name' => 'Celtniecībai',
+        'image' => file_get_contents("images/groups/helmet.png"),
+      ]);
+      \DB::table('groups')->insert([
+        'ID' => 10,
+        'group_name' => 'Dārzam',
+        'image' => file_get_contents("images/groups/gardening.png"),
+      ]);
         // \App\Models\User::factory(10)->create();
     }
 }
